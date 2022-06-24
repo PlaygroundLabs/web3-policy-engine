d0a049bc7638a8d23c44a88071a041efc0afe3aa
Daniel Neshyba-Rowe <daniel.rowe@playgroundlabs.io>
2022-06-24 12:21:13 -0700

    added some basic yaml reading and permission processing

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,15 +1,93 @@
 from unittest import TestCase
 from hexbytes import HexBytes
+from yaml import safe_load
 
 from web3_policy_engine.schemas import Transaction
 from web3_policy_engine.parse_transaction import Parser
 
 
-class TestWeb3(TestCase):
-    def test_parser(self):
-        parser = Parser(["multiply(uint256)"])
+class TestParser(TestCase):
+    def test_address(self):
+        parser = Parser(
+            {
+                HexBytes("0x1234123412341234123412341234123412341234"): {
+                    "multiply(uint256)": ["role1"]
+                }
+            }
+        )
         transaction = Transaction(
-            HexBytes("0x6ff93b4b46b41c0c3c9baee01c255d3b4675963d"),
-            HexBytes("0xc6888fa10000000000000000000000000000000000000000000000000000000000000006"),
+            HexBytes("0x1234123412341234123412341234123412341234"),
+            HexBytes(
+                "0xc6888fa10000000000000000000000000000000000000000000000000000000000000006"
+            ),
+        )
+        self.assertTrue(parser.is_valid_address(transaction))
+
+        transaction = Transaction(
+            HexBytes("0x8888888888888888888888888888888888888888"),
+            HexBytes("0x0"),
+        )
+        self.assertFalse(parser.is_valid_address(transaction))
+
+    def test_method(self):
+        parser = Parser(
+            {
+                HexBytes("0x1234123412341234123412341234123412341234"): {
+                    "multiply(uint256)": ["role1"]
+                }
+            }
+        )
+        transaction = Transaction(
+            HexBytes("0x1234123412341234123412341234123412341234"),
+            HexBytes(
+                "0xc6888fa10000000000000000000000000000000000000000000000000000000000000006"
+            ),
         )
         self.assertTrue(parser.is_valid_method(transaction))
+
+        transaction = Transaction(
+            HexBytes("0x1234123412341234123412341234123412341234"),
+            HexBytes("0x909090909"),
+        )
+        self.assertFalse(parser.is_valid_method(transaction))
+
+        transaction = Transaction(
+            HexBytes("0x9999999999999999999999999999999999999999"),
+            HexBytes(
+                "0xc6888fa10000000000000000000000000000000000000000000000000000000000000006"
+            ),
+        )
+        # self.assertRaises(ValueError, parser.is_valid_method, transaction)
+        self.assertFalse(parser.is_valid_method(transaction))
+    
+    def test_role(self):
+        parser = Parser(
+            {
+                HexBytes("0x1234123412341234123412341234123412341234"): {
+                    "multiply(uint256)": ["role1"]
+                }
+            }
+        )
+        transaction = Transaction(
+            HexBytes("0x1234123412341234123412341234123412341234"),
+            HexBytes(
+                "0xc6888fa10000000000000000000000000000000000000000000000000000000000000006"
+            ),
+        )
+        self.assertTrue(parser.is_valid_role(transaction, ["role1"]))
+        self.assertFalse(parser.is_valid_role(transaction, ["role2"]))
+        self.assertTrue(parser.is_valid_role(transaction, ["role2", "role1"]))
+        self.assertFalse(parser.is_valid_role(transaction, []))
+
+
+    def test_load_from_yaml(self):
+        with open("data/local_data/test_config.yml", "r") as file_handle:
+            yaml_data = safe_load(file_handle)
+            # convert from str to HexBytes
+            yaml_data = {HexBytes(key):value for key,value in yaml_data.items()}
+            parser = Parser(yaml_data)
+            transaction = Transaction(
+                HexBytes("0x2222222222222222222222222222222222222222"),
+                HexBytes("0xc6888fa1"),
+            )
+            self.assertTrue(parser.is_valid_address(transaction))
--- web3_policy_engine/parse_transaction.py
+++ web3_policy_engine/parse_transaction.py
@@ -1,17 +1,48 @@
+from multiprocessing.sharedctypes import Value
 from web3 import Web3
 from hexbytes import HexBytes
 
 from .schemas import Transaction
 
-class Parser:
-    def __init__(self, methods : list[str]):
-        self.method_prefix_len = 4 # bytes
-        self.method_names = methods
-        self.methods = [self.name_to_bytes(name) for name in methods]
 
-    def name_to_bytes(self, name : str) -> HexBytes:
-        return Web3.keccak(text=name)[:self.method_prefix_len]
-    
-    
-    def is_valid_method(self, transaction : Transaction):
-        return transaction.data[:self.method_prefix_len] in self.methods
+class Parser:
+    def __init__(self, allowed: dict[HexBytes, dict[str, list[str]]]):
+        self.method_prefix_len = 4  # bytes
+        self.method_names = allowed
+        self.method_hashes = {
+            address: {
+                self.name_to_hash(method): roles for method, roles in methods.items()
+            }
+            for address, methods in allowed.items()
+        }
+
+    def name_to_hash(self, name: str) -> HexBytes:
+        return Web3.keccak(text=name)[: self.method_prefix_len]
+
+    def is_valid_address(self, transaction: Transaction):
+        return transaction.to in self.method_names
+
+    def is_valid_method(self, transaction: Transaction):
+        if not self.is_valid_address(transaction):
+            # TODO: decide what to do in this case.
+            # Does it make more sense to return False, or to raise error?
+            # raise ValueError("Invalid address")
+            return False
+        return (
+            transaction.data[: self.method_prefix_len]
+            in self.method_hashes[transaction.to]
+        )
+
+    def is_valid_role(self, transaction: Transaction, roles: list[str]):
+        if not self.is_valid_method(transaction):
+            # raise ValueError("Invalid method")
+            return False
+        return any(
+            [
+                role
+                in self.method_hashes[transaction.to][
+                    transaction.data[: self.method_prefix_len]
+                ]
+                for role in roles
+            ]
+        )
